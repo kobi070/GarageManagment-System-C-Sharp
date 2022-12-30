38c9771d18d9517ef62f0bc3049c399ffd307f0d
kobi070 <kobi070@gmail.com>
2022-12-31 01:00:07 +0200

    Second Commit

--- Ex03.GarageLogic/Customer.cs
+++ Ex03.GarageLogic/Customer.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
+
+    public enum eServeiceStatus
+    {
+        InRepair = 1,
+        Fixed,
+        Paid
+    }
     internal class Customer
     {
+        private readonly string r_Name;
+        private readonly string r_PhoneNumber;
+        private readonly Vehicle r_Vechile;
+        private eServeiceStatus m_VechileStatus;
+
+        internal Customer(string i_Name, string i_PhoneNumber, Vehicle i_Vehicle, eServeiceStatus i_VehicleStatus)
+        {
+            this.r_Name = i_Name;
+            this.r_PhoneNumber = i_PhoneNumber;
+            this.r_Vechile = i_Vehicle;
+            this.m_VechileStatus = i_VehicleStatus;
+        }
+        internal string Name 
+        {
+            get 
+            {
+                return r_Name; 
+            }
+        }
+        internal string PhoneNumber 
+        {
+            get 
+            { 
+                return r_PhoneNumber; 
+            }
+        }
+        internal Vehicle Vehicle 
+        {
+            get { return r_Vechile; }
+        }
     }
 }
