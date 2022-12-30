7158ac94596f40a9439bbcf7ca5284ab8605c092
kobi070 <kobi070@gmail.com>
2022-12-31 01:47:35 +0200

    Fourth Commit

---
+++ Ex03.GarageLogic/Garage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Garage
+    {
+        private readonly Dictionary<string, Customer> r_Customers = new Dictionary<string, Customer>();
+
+        internal Garage(string io_CustomerName, string io_PhoneNumber, string io_VehicleSerialNumber, int io_serveiceStatus )
+        {
+            bool alredyExist = false;
+
+            if (isExist(io_VehicleSerialNumber))
+            {
+                try
+                {
+                    this.r_Customers[io_VehicleSerialNumber].VehicleStatus = eServeiceStatus.InRepair;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(String.Format($"{io_VehicleSerialNumber} is not in the system"), ex);
+                }
+            }
+            else
+            {
+                if (Enum.IsDefined(typeof(), io_serveiceStatus))
+                {
+                    ///...
+                }
+
+            }
+        }
+        internal bool isExist(string io_LicenseNumber)
+        {
+            return this.r_Customers.ContainsKey(io_LicenseNumber);
+        }
+    }
+}
